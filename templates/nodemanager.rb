92848ebb6afeb41436646c4cae88328871d2fd71
Alex Chistyakov <alexclear@gmail.com>
2016-04-01 20:45:49 +0300

    Use uid directive

--- templates/nodemanager.rb
+++ templates/nodemanager.rb
@@ -3,10 +3,11 @@ Eye.application 'yarn-nodemanager-{{ env_name }}' do
   stdall '/var/log/eye/yarn-nodemanager-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  uid "{{ hadoop_user }}"
 
   process :nodemanager_{{ env_name }} do
     pid_file '{{ hadoop_var_prefix }}/yarn_nodemanager.pid'
-    start_command 'sudo -u {{ hadoop_user }} {{ hadoop_distr_prefix }}/bin/yarn nodemanager'
+    start_command '{{ hadoop_distr_prefix }}/bin/yarn nodemanager'
 
     daemonize true
     start_timeout 10.seconds
