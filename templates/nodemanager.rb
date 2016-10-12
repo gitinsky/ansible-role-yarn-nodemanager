d8bac16f29e1b3d270b3b3d6bd364e92ba72c638
Alex Chistyakov <alexclear@gmail.com>
2016-10-12 13:31:37 +0300

    Run YARN using a separate user

--- templates/nodemanager.rb
+++ templates/nodemanager.rb
@@ -3,7 +3,7 @@ Eye.application 'yarn-nodemanager-{{ env_name }}' do
   stdall '/var/log/eye/yarn-nodemanager-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
-  uid "{{ hadoop_user }}"
+  uid "{{ yarn_user }}"
 
   process :nodemanager_{{ env_name }} do
     pid_file '{{ hadoop_var_prefix }}/yarn_nodemanager.pid'
